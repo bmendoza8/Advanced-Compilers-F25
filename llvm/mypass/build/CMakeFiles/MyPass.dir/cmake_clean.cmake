a1bf4d26de64e8d4d6c1c48db81ea218091be942
Brayan Mendoza <bmendoza8@miners.utep.edu.com>
2025-11-15 21:06:55 -0700

    Full LLVM pass project + build directory

---
+++ llvm/mypass/build/CMakeFiles/MyPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyPass.dir/mypass.cpp.o"
+  "CMakeFiles/MyPass.dir/mypass.cpp.o.d"
+  "MyPass.pdb"
+  "MyPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
